855c3f32e108d11c9b3239568835b7d379b6877c
Mariano Cárcamo <marianocarcamo98@gmail.com>
2024-06-01 18:03:08 -0300

    feat: Incluye hasheo de password

--- Desafio_5/src/routes/api/session.js
+++ Desafio_5/src/routes/api/session.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import User from '../../dao/models/user.model.js';
+import { createHash, isValidPassword } from '../../utils.js';
 
 const router = Router();
 
@@ -15,7 +16,7 @@ const ADMIN = {
 router.post('/register', async (req, res) => {
     const { first_name, last_name, email, age, password } = req.body;
     try {
-        const newUser = new User({ first_name, last_name, email, age, password });
+        const newUser = new User({ first_name, last_name, email, age, password: createHash(password) });
         newUser.rol = "usuario"
         await newUser.save();
         res.redirect('/login');
@@ -26,12 +27,15 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
+    if( !email || !password ) return res.status(400).send({status:"error", error:"Datos incompletos"})
     try {
         if (email === ADMIN.email && password === ADMIN.password) {
             req.session.user = ADMIN
         } else {
-            const user = await User.findOne({ email ,password });
+            const user = await User.findOne({ email }, {email:1, first_name:1, last_name:1, password:1});
             if (!user) return res.status(404).send('Usuario no encontrado');
+            if ( !isValidPassword(user, password) ) return res.status(403).send({status:"error", error: "Contraseña incorrecta"});
+                delete user.password
             req.session.user = {
                 id: user._id,
                 first_name: user.first_name,
