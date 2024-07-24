863c602c6510d417580af7f08afe820473fd72fc
Mariano Cárcamo <marianocarcamo98@gmail.com>
2024-07-23 23:01:11 -0300

    feat: Implementa desafio 8 - mocking

--- Desafio_8/src/app.js
+++ Desafio_8/src/app.js
@@ -4,16 +4,24 @@ import handlebars from 'express-handlebars'
 import mongoose from 'mongoose'
 import MongoStore from 'connect-mongo'
 import passport from 'passport'
+import { Server } from 'socket.io'
 import __dirname from './utils.js'
 import config from './config/config.js'
+import chatSocket from './services/chatSocket.service.js'
 import sessionRouter from './routes/api/session.router.js'
 import productsRouter from './routes/api/products.router.js'
 import cartsRouter from './routes/api/carts.router.js'
 import viewsRouter from './routes/views.js'
+import mockRouter from './routes/mock.router.js'
 import initializePassport from './config/passport.config.js'
+import errorHandler from './middleware/errors/index.js'
 
 const PORT = config.port
 const app = express()
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server's up and running on port ${PORT}`)
+})
+const socketServer = new Server(httpServer)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -21,6 +29,7 @@ app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
+app.set('socketServer', socketServer)
 app.use(express.static(__dirname + '/public/'))
 
 mongoose
@@ -28,12 +37,14 @@ mongoose
     .then(() => console.log('Conectado a la base de datos'))
     .catch((error) => console.error('Error en la conexion', error))
 
-app.use( session ({
-    secret: config.secret,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: config.mongoUrl })
-}))
+app.use(
+    session({
+        secret: config.secret,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: config.mongoUrl }),
+    })
+)
 
 initializePassport()
 app.use(passport.initialize())
@@ -42,8 +53,8 @@ app.use(passport.session())
 app.use('/api/sessions', sessionRouter)
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
+app.use('/mockingproducts',mockRouter)
 app.use('/', viewsRouter)
+// app.use(errorHandler)
 
-app.listen(PORT, () => {
-    console.log(`Server's up and running on port ${PORT}`)
-})
+chatSocket(socketServer)
